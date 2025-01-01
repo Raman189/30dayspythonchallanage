967077eb4cfb4d9e3f18a718b59e2695886b9eba
Raman <ramanvanjare447@gmail.com>
2025-01-01 11:26:07 +0530

    first

---
+++ Day01.py
@@ -0,0 +1,13 @@
+# hello frieds from today 1st jan 2025 i started 30days python challanage 
+# Today I write my first python program 
+# The print() function prints the specified message to the screen  
+
+print("Hello World !!")
+
+# here we learn more about python that is variables
+
+x = 34          # here "x" is a variable and we assign it's value 34
+name = "Ram"    # same here varible = name , value = "Ram"
+
+print(x)       # here we print our varibles value by using print statement 
+print(name)    # here we print our varibles value by using print statement
